bdda5e8bb7a579058a8959cdeca2afe2c014cb81
ricardospinoza <ricardospinoza@gmail.com>
2024-11-07 21:09:31 -0300

    melhoria nas missões e tentativa antecipar ataque para fugir

--- tests/bot_farm.py
+++ tests/bot_farm.py
@@ -4,6 +4,7 @@ import os
 import ctypes
 import sys
 import threading
+from datetime import datetime
 
 # Disable the fail-safe feature
 pyautogui.FAILSAFE = False
@@ -34,12 +35,13 @@ def start_animation():
         animate_rotating_chars(0.5)
 
 # List of image filenames
-image_filenames = [
+image_filenames = [    
     "missao_concluida.jpg",
     "missao_aceita.jpg",    
     "pular_bla.jpg",
     "pular_blabla.jpg",
     "pular_blablabla.jpg",
+    "limite_campanha_diaria_ignorar_missao.jpg",
     "teletransportar.jpg",
     "diretiva_disponivel_guilda.jpg",
     "guilda_atualizar_diretivas.jpg",    
@@ -53,34 +55,57 @@ image_filenames = [
     "teletransportar.jpg",
     "porcao_zerada.jpg",
     "sendo_atacado.jpg",
+    "sendo_atacado_2.jpg",
     "sobre_ataque.jpg",
     "saida_masmorra.jpg",
     "teletransportar.jpg",
-    "equipe_ok.jpg"
+    "equipe_ok.jpg"    
 ]
 
-def execute():
+# Variável para armazenar a data em que limite_campanha_diaria foi detectada
+limite_detectado_data = None
 
+def execute():
     # Inicia a animação em uma thread separada
     threading.Thread(target=start_animation, daemon=True).start()
 
     while True:
+        global limite_detectado_data
         is_sair_guilda = False
         is_sair_masmorra = False
 
+        # Obtém a data atual
+        hoje = datetime.now().date()
+
+        # Verifica se já detectou limite_campanha_diaria hoje
+        if limite_detectado_data == hoje:
+            ignore_missao_aceita = True
+        else:
+            ignore_missao_aceita = False
+
         for image_filename in image_filenames:
-            # Ensure the path to the image is correct
+            # Ignora "missao_aceita.jpg" se já detectou "limite_campanha_diaria_ignorar_missao.jpg" no dia atual
+            if ignore_missao_aceita and (image_filename == "missao_aceita.jpg" or image_filename == "diretiva_disponivel_guilda.jpg"):
+                continue
+
+            # Caminho para a imagem
             image_path = os.path.join(os.path.dirname(__file__), image_filename)
-            #print(f"Procurando imagem: {image_path}")
 
             try:
                 image_location = pyautogui.locateOnScreen(image_path, confidence=0.8)
                 if image_location:
-                    if image_filename in ["guilda_400.jpg", "guilda_240.jpg"]:
+                    # Se "limite_campanha_diaria_ignorar_missao.jpg" for detectada, salva a data
+                    if image_filename == "limite_campanha_diaria_ignorar_missao.jpg":
+                        limite_detectado_data = hoje
+                        print("Limite de campanha diária detectado. Ignorando missões para o dia.")
+
+                    elif image_filename in ["guilda_400.jpg", "guilda_240.jpg"]:
                         pyautogui.click(577, 592)
                         is_sair_guilda = True
-                    elif image_filename in ["porcao_zerada.jpg", "sendo_atacado.jpg", "sobre_ataque.jpg"] and not is_sair_masmorra:                        
+
+                    elif image_filename in ["porcao_zerada.jpg", "sendo_atacado.jpg", "sobre_ataque.jpg"] and not is_sair_masmorra:
                         is_sair_masmorra = True
+
                     else:
                         image_name, _ = os.path.splitext(image_filename)
                         print(f"Detectada acao: {image_name}                         ")
@@ -94,15 +119,13 @@ def execute():
                             is_sair_masmorra = False
                             continue
 
-                        if image_filename in ["porcao_zerada.jpg", "sendo_atacado.jpg", "sobre_ataque.jpg"]:
+                        if image_filename in ["porcao_zerada.jpg", "sendo_atacado.jpg", "sobre_ataque.jpg", "sendo_atacado_2.jpg"]:
                             continue
                         
                         pyautogui.click(x, y)
                         time.sleep(0.5)
-                #else:
-                #    print(f"Imagem não encontrada: {image_filename}")
+
             except pyautogui.ImageNotFoundException:
-                #print(f"Exceção de imagem não encontrada: {image_filename}")
                 pass
 
         #animate_rotating_chars(0.5)
